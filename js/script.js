f8f327704dbae9998ea7ad1087657af61c496886
HAYASHInnn <fffm572752@gmail.com>
2025-03-09 22:45:00 +0900

    ・ページトップへ戻るボタンを付ける 　→ スクロールしたらフェードイン・アウトで表示

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
+// ドロワーメニュー
 $("#js-menu").on("click", function () {
   $("#js-drawer").slideToggle();
 });
 
+// スクロールしたら、フェードイン・フェードアウト
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 80) {
+    $("#page_top").fadeIn(300);
+  } else {
+    $("#page_top").fadeOut(300);
+  }
+});
 
+// クリックしたらページトップ
+$("#page_top").click(function (e) {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: 0 }, 500);
+});
